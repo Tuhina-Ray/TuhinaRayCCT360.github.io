07b794ff9acf46fea68380e509f7b0b54e909fee
Tuhina-Ray <tuhinaray03@gmail.com>
2026-09-23 16:19:10 -0400

    updating lab 2

---
+++ Labs/L2_EventsInMotion/js/external.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("butClick").onclick = function() {alert("Heres an Alert");
+}
+
+document.getElementById("popClick").onclick = function (){
+    window.open("popup.html", "", "width=200, height=200");
+
+}
